1a4d56e27135bc4ad8ba77963afa5644090d7598
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-04 11:08:19 +0100

    exercise 3e

---
+++ src/chapter3/GradeMessage.java
@@ -0,0 +1,28 @@
+package chapter3;
+
+/*
+ * SWITCH
+ * Have a user enter their letter grade, and using a switch statement,
+ * print out a message letting them know how they did.
+ */
+
+import java.util.Scanner;
+
+public class GradeMessage {
+    public static void main(String[] args) {
+        System.out.println("Enter your letter grade:");
+        Scanner scanner = new Scanner(System.in);
+        String grade = scanner.next();
+        scanner.close();
+        String message = switch (grade) {
+            case "A" -> "Excellent job!";
+            case "B" -> "Great job!";
+            case "C" -> "Good job!";
+            case "D" -> "You need to work a bit harder!";
+            case "F" -> "Uh oh!";
+            default -> "Wrong grade!";
+        };
+
+        System.out.println(message);
+    }
+}
