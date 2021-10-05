95d88b632ea408f87f72ec41ce034ebb09094a2e
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-05 21:44:33 +0100

    exercise 6dii

---
+++ src/chapter6/MonthConverter.java
@@ -0,0 +1,8 @@
+package chapter6;
+
+public class MonthConverter {
+    public static void main(String[] args) {
+        System.out.println(Month.getMonth(5));
+        System.out.println(Month.getMonth("May"));
+    }
+}
