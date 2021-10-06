75d9a603c81e9259319320f19e2985f373816c71
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-06 17:26:39 +0100

    exercise 8bi

---
+++ src/chapter8/TextProcessor.java
@@ -0,0 +1,27 @@
+package chapter8;
+
+
+
+public class TextProcessor {
+    public static void main(String[] args) {
+        countWords("I love Test Automation University");
+    }
+
+    /**
+     * Splits a String into an array by tokenizing it.
+     * Counts words and prints them
+     * @param text Full string to be split
+     */
+
+    private static void countWords(String text) {
+        var words = text.split(" ");
+        int numberOfWords = words.length;
+
+        String message = String.format("Your text should contain %d words:", numberOfWords);
+        System.out.println(message);
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
