0b7b65d7509370b7e9eadb6833a18ab767ff8351
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-06 18:07:43 +0100

    independent exercise

---
+++ src/chapter8/PasswordValidator.java
@@ -0,0 +1,14 @@
+package chapter8;
+
+/*
+Validate the complexity of a proposed password by assuring it meets these rules:
+- at least 8 characters long
+- contains an uppercase letter
+- contain a special character
+- not contain the username
+- not the same as the old password
+ */
+
+public class PasswordValidator {
+
+}
