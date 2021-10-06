f12559fd2619ae53ac737f3331183bb80fe54133
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-06 17:54:23 +0100

    exercise 8biii

--- src/chapter8/TextProcessor.java
+++ src/chapter8/TextProcessor.java
@@ -1,11 +1,10 @@
 package chapter8;
 
-
-
 public class TextProcessor {
     public static void main(String[] args) {
         countWords("I love Test Automation University");
         reverseString("Hello TAU!");
+        addSpaces("HeyWorld!It'sMeAngie");
     }
 
     /**
@@ -35,5 +34,23 @@ public class TextProcessor {
         for(int i = text.length() - 1; i >= 0; i--){
             System.out.print(text.charAt(i));
         }
+        System.out.println();
+    }
+
+    /**
+     * Adds spaces before each uppercase letter
+     * @param text jumbled text
+     */
+
+    private static void addSpaces(String text) {
+        var modifiedText = new StringBuilder(text);
+        for (int i = 0; i < modifiedText.length(); i++) {
+            //we want the spaces added after the first word, so we skip the first index
+            if(i != 0 && Character.isUpperCase(modifiedText.charAt(i))){
+                modifiedText.insert(i, " ");
+                i++;
+            }
+        }
+        System.out.println(modifiedText);
     }
 }
