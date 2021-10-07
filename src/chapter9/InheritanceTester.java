496c3cdd191dccf49377156a7f42409f20959047
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-07 11:53:51 +0100

    exercise 9b

--- src/chapter9/InheritanceTester.java
+++ src/chapter9/InheritanceTester.java
@@ -2,9 +2,9 @@ package chapter9;
 
 public class InheritanceTester {
     public static void main(String[] args) {
-        Person person = new Person();
+//        Person person = new Person();
         Employee employee = new Employee();
-        person.getAge();
+//        person.getAge();
         employee.getAge(); //employee has access to member of person class because it inherits from person
     }
 }
