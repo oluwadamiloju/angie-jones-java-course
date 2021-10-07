16101d14f5da5e5c8c4616afec193d4b47ef066c
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-07 13:47:29 +0100

    exercise 9a, b, d

--- src/chapter9/person/InheritanceTester.java
+++ src/chapter9/person/InheritanceTester.java
@@ -5,8 +5,12 @@ import chapter9.person.Employee;
 public class InheritanceTester {
     public static void main(String[] args) {
 //        Person person = new Person();
-        Employee employee = new Employee();
+//        Employee employee = new Employee();
 //        person.getAge();
-        employee.getAge(); //employee has access to member of person class because it inherits from person
+//        employee.getAge(); //employee has access to member of person class because it inherits from person
+        Mother mom  = new Mother();
+        mom.setName("Adeyinka");
+
+        System.out.println(mom.getName() + " is a " + mom.getGender());
     }
 }
