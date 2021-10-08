7a399cae60a7e95c6b68be66538fc818d66ced0d
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-08 11:21:23 +0100

    exercise 11a

--- src/chapter11/shapes/ShapeTester.java
+++ src/chapter11/shapes/ShapeTester.java
@@ -1,2 +1,9 @@
-package chapter11.shapes;public class ShapeTester {
+package chapter11.shapes;
+
+public class ShapeTester {
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle(5,7);
+        rectangle.print();
+        System.out.println(rectangle.calculateArea());
+    }
 }
