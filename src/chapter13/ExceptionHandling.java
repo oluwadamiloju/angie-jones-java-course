0a81c7c5df75dc01bdc054d9e00f6ae9a42c836c
Sarah Akinkunmi <sarah.akinkunmi@stu.cu.edu.ng>
2021-10-08 16:50:14 +0100

    exercise 13a

---
+++ src/chapter13/ExceptionHandling.java
@@ -0,0 +1,37 @@
+package chapter13;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+//        createANewFile();
+        numbersExceptionHandling();
+    }
+
+    private static void numbersExceptionHandling() {
+        File file = new File("resources/numbers.txt");
+
+        try(Scanner fileReader = new Scanner(file)) {
+            while(fileReader.hasNext()) {
+                double num = fileReader.nextDouble();
+                System.out.println(num);
+            }
+        } catch (FileNotFoundException | InputMismatchException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private static void createANewFile() {
+        File file = new File("resources/nonexistent.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e){
+            System.out.println("Directory does not exist");
+            e.printStackTrace();
+        }
+    }
+}
